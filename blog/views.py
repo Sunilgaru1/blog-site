fa35fcb842542a73f0723e920d242a919ffc1f3e
sunilgaru1 <sunilgaru097@gmail.com>
2026-06-11 14:55:22 +0530

    Added Views for Blog App

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import BlogModel
 # Create your views here.
+
+def BlogView(request):
+    blogdata = BlogModel.objects.all()
+
+    return render(request,'blog/blog_index.html',{'blogdata':blogdata})
