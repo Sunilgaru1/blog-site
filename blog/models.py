eca2464358ccad0d14464affbeb6a0de1d3f0ba5
sunilgaru1 <sunilgaru097@gmail.com>
2026-06-11 14:31:49 +0530

    Added Model to Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class BlogModel(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
